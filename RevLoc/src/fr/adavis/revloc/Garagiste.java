a1b3716e16686b5feef1464d9db3f18ac2927cfa
Charlotte <charlotte.picois.sio@gmail.com>
2016-11-28 16:43:43 +0100

    Constructeur Garagiste

--- RevLoc/src/fr/adavis/revloc/Garagiste.java
+++ RevLoc/src/fr/adavis/revloc/Garagiste.java
@@ -5,6 +5,14 @@ public class Garagiste {
 		private String nom;
 		private String adresse;
 		
+		public Garagiste(String nom, String adresse) {
+			super();
+			this.nom = nom;
+			this.adresse = adresse;
+		}
+		
+		
+		
 	
 	
 }
